f2424337779f0cdf0a9421158c0e67ceb8268160
WhoNew <22082099+18anguyen33@users.noreply.github.com>
2021-04-24 22:48:19 -0700

    Main Win conditions
    Added computer object and added win if statements

--- Main.java
+++ Main.java
@@ -1,20 +1,33 @@
 import java.util.Scanner;
 public class Main {
     
-	static colPos
+	static int colPos
 	static GameState gameBoard;
-	static boolean win = false;
+	static MinMax com;
 	
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		System.out.println("New Connect 4 Game has started");
 		gameBoard = new GameState();
 		gameBoard.boardPrint();
-		while(!win){
+		com = newMinMax(GameState.O);
+		while(!gameBoard.ifGameOver()){
+		        System.out.println();
 				System.out.println("Select Column to drop piece");
 				colPos = in.nextInt();
 				gameBoard.makeMovement(colPos, GameState.player);
 				gameBoard.boardPrint();
+		}
+
+		System.out.println();
+		if (gameBoard.victor == GameState.X){
+		    System.out.println("The Player wins. Congratulations!");
+		}
+		else if (gameBoard.victor == GameState.O){
+		    System.out.println("The Computer wins. Better Luck Next time.");
+		}
+		else {
+		    System.out.println("Its a draw. Better luck next time.");
 		}
 	}	
 
