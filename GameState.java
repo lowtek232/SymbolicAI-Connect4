8094767c5f63178d1d69291d8d8792d31f116e4f
WhoNew <22082099+18anguyen33@users.noreply.github.com>
2021-04-24 22:54:10 -0700

    Flesh Out GameState More
    added more movement methods and fleshed out the current methods

--- GameState.java
+++ GameState.java
@@ -2,8 +2,8 @@ public class GameState {
     int board[][];
     static final int X = 1;
     static final int O = -1;
-    int NILL = 0;
     GamePlay lastPlay;
+    int NILL = 0;
     int lastLetterUsed;
     int victor;
     String victorMethod;
@@ -20,18 +20,49 @@ public class GameState {
         }
     }
 
-    public void mutateVictor(int victor){
-        this.victor = victor;
+    public boolean ifWin(){
+        for (int h=5;h>=0;h--){
+            for (int i=0;i<4;i++){
+                if (board[h][i] == board[h][i+1] && board[h][i] == board[h][i+2] && board[h][i] == board[h][i+2] && board[h][i] != NILL){
+                    mutateVictor(board[h][i]);
+                    mutateVictorMethod("4 in a row on row "+h);
+                    return true;
+                }
+            }
+        }
+
+        for (int h=5;h>=3;h--){
+            for(int i=0;i<7;i++){
+                if (board[h][i] == board[h-1][i] && board[h][i] == board[h-2][i] && board[h][i] == board[h-3][i] && board[h][i] != NILL){
+                    mutateVictor(board[h][i]);
+                    mutateVictorMethod("4 in a row on column "+i);
+                    return true;
+                }
+            }
+        }
+
+        for (int h=0;h<3>;h++){
+            for(int i=0;i<4;i++){
+                if (board[h][i] == board[h+1][i+1] && board[h][i] == board[h+2][i+2] && board[h][i] == board[h+3][i+3] && board[h][i] != NILL){
+                    mutateVictor(board[h][i]);
+                    mutateVictorMethod("4 in a row on diagonal");
+                    return true;
+                }
+            }
+        }
+
+        for (int h=0;h<6>;h++){
+            for(int i=0;i<7;i++){
+                if (moveable(h-3,i+3){
+                    if (board[h][i] == board[h-1][i+1] && board[h][i] == board[h-2][i+2] && board[h][i] == board[h-3][i+3] && board[h][i] != NILL){
+                        mutateVictor(board[h][i]);
+                        mutateVictorMethod("4 in a row on diagonal");
+                        return true;
+                    }
+                }
+            }
+        }
     }
-
-    public boolean moveable(int row, int col){
-
-    }
-
-    public boolean checkFullColumn(int col){
-
-    }
-
 
     public void boardPrint(){
         for(int i=0; i<6; i++){
@@ -47,5 +78,26 @@ public class GameState {
             System.out.println("| " + "-" + " ");
         }
         System.out.print("- 1 - 2 - 3 - 4 - 5 - 6 - 7 -");
+    }
+
+    public void mutateVictor(int victor){
+        this.victor = victor;
+    }
+
+    public void mutateVictorMethod(String method){
+        this.victorMethod = method;
+    }
+
+    public void makeMovement(int c, int let){
+        lastPlay = lastPlay.finishMove(getRow(c), c);
+        board[getRow(c)][c] = let;
+        lastLetterUsed = let;
+    }
+
+    public boolean moveable(int r, int c){
+        return true;
+    }
+    public boolean checkFullColumn(int col){
+        return false;
     }
 }
