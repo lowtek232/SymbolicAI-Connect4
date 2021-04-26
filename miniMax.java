8ec6d7b2f80a8239b3a667890d06562335a4d5f6
Kenny Chhoeun <59298127+lowtek232@users.noreply.github.com>
2021-04-26 08:20:36 -0700

    Update to the minumumAlgorithm

--- miniMax.java
+++ miniMax.java
@@ -1,4 +1,9 @@
 import java.util.ArrayList;
+import java.util.Random;
+
+/*
+ * @ Authors: Kenny Chhoeun, Javier Meza
+ */
 
 public class miniMax {
 	int maximumDepth, aiPlayerID;
@@ -18,15 +23,36 @@ public class miniMax {
 		} else {
 			ArrayList<GameState> childen = new ArrayList<GameState>();
 			// create another gameplay object that represenet the mininum move
-			Gameplay mininumMove = new Gameplay();
-			/* for everything under the size of the children get
-			 * and get the value for what is higher value the child or lower depth
-			 * the one with lowest is chosen and returned
-			 * 
-			 */
-			
+			Gameplay minimumMove = new Gameplay();
+			Gameplay move;
+			minumumMove = minumumMove.compareMove(2147483647);
+			int size = children.size();
+			for(int i=0; i<size;i++) {
+				GameState child = children.get(i);
+				move = max(child, depth+1);
+				/* for everything under the size of the children get
+				 * and get the value for what is higher value the child or lower depth
+				 * the one with lowest is chosen and returned
+				 * 
+				 */
+				if(minimumMove.key() > move.key()) {
+					if((move.key() == minimumMove.key())) {
+						// if the values of these are the same just randomly pick one of them
+						// random number 0 or 1
+						if(random.nextInt(2) == 0) {
+							minimumMove.mutateRows(child.lastPlay.row);
+							minimumMove.mutateColumns(child.lastPlay.col);
+							minimumMove.mutateKey(child.getKey());
+						}
+					} else {
+						minimumMove.mutateRows(child.lastPlay.row);
+						minimumMove.mutateColumns(child.lastPlay.col);
+						minimumMove.mutateKey(child.getKey());
+					}
+				}
+			}
+			return minimumMove;
 		}
-		// if not then calculate all the child moves
 	} // end of min function
 
 	//MAXIMUM METHOD
