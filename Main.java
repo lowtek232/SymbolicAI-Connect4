806db3a44ca9ce8a752777d7b9a55d8a2b524501
milotash <83149550+milotash@users.noreply.github.com>
2021-04-25 23:46:56 -0700

    Updated and completed Main.java
    Added while loop for player/com turns

--- Main.java
+++ Main.java
@@ -7,28 +7,54 @@ public class Main {
 	
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
-		System.out.println("New Connect 4 Game has started");
+		System.out.println("Connect 4 Game has started");
 		gameBoard = new GameState();
 		gameBoard.boardPrint();
-		com = newMinMax(GameState.O);
+		com = new MinMax(GameState.O);
+		
 		while(!gameBoard.ifGameOver()){
-		        System.out.println();
-				System.out.println("Select Column to drop piece");
-				colPos = in.nextInt();
-				gameBoard.makeMovement(colPos, GameState.player);
-				gameBoard.boardPrint();
+			System.out.println();
+			switch (gameBoard.lastLetterUsed) {
+				case GameState.X:
+					System.out.println("Computer O's turn.");
+					GamePlay comMove = com.nextMove(gameBoard);
+					gameBoard.makeMovement(comMove.columns, GameState.O);
+					System.out.println("Computer O played on column " + (comMove.columns + 1) + ".");
+					System.out.println();
+					break;
+				case GameState.O:
+		        	System.out.println("Player X's turn.");
+		        	try {
+		        		do {
+		    				System.out.println("\nSelect Column (1-7) to drop piece: ");
+		    				colPos = in.nextInt();
+		        		} while (gameBoard.isColFull(colPos - 1));
+		        	} catch (Exception e) {
+		        		System.out.println("\nNumber not in range of 1-7. Try again.\n");
+		        		break;
+		        	}
+		        	gameBoard.makeMovement(colPos - 1, GameState.X);
+		        	gameBoard.boardPrint();
+		        	break;
+				default:
+					break;
+		        }
+			gameBoard.boardPrint();
 		}
 
 		System.out.println();
 		if (gameBoard.victor == GameState.X){
 		    System.out.println("The Player wins. Congratulations!");
+		    System.out.println(gameBoard.victorMethod);
 		}
 		else if (gameBoard.victor == GameState.O){
 		    System.out.println("The Computer wins. Better Luck Next time.");
+		    System.out.println(gameBoard.victorMethod);
 		}
 		else {
 		    System.out.println("Its a draw. Better luck next time.");
 		}
+		System.out.println("Game over.");
 	}	
 
 }
