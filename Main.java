5ad05229f9ac02dc9cf4bb65150eee6aaf316f00
WhoNew <22082099+18anguyen33@users.noreply.github.com>
2021-04-26 22:06:32 -0700

    New changes to Main Classs
    These changes are to accommodate for the gui.

--- Main.java
+++ Main.java
@@ -1,60 +1,33 @@
-import java.util.Scanner;
 public class Main {
     
-	static int colPos
-	static GameState gameBoard;
-	static MinMax com;
-	
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		System.out.println("Connect 4 Game has started");
-		gameBoard = new GameState();
-		gameBoard.boardPrint();
-		com = new MinMax(GameState.O);
-		
-		while(!gameBoard.ifGameOver()){
-			System.out.println();
-			switch (gameBoard.lastLetterUsed) {
-				case GameState.X:
-					System.out.println("Computer O's turn.");
-					GamePlay comMove = com.nextMove(gameBoard);
-					gameBoard.makeMovement(comMove.columns, GameState.O);
-					System.out.println("Computer O played on column " + (comMove.columns + 1) + ".");
-					System.out.println();
-					break;
-				case GameState.O:
-		        	System.out.println("Player X's turn.");
-		        	try {
-		        		do {
-		    				System.out.println("\nSelect Column (1-7) to drop piece: ");
-		    				colPos = in.nextInt();
-		        		} while (gameBoard.isColFull(colPos - 1));
-		        	} catch (Exception e) {
-		        		System.out.println("\nNumber not in range of 1-7. Try again.\n");
-		        		break;
-		        	}
-		        	gameBoard.makeMovement(colPos - 1, GameState.X);
-		        	gameBoard.boardPrint();
-		        	break;
-				default:
-					break;
-		        }
-			gameBoard.boardPrint();
-		}
-
-		System.out.println();
-		if (gameBoard.victor == GameState.X){
-		    System.out.println("The Player wins. Congratulations!");
-		    System.out.println(gameBoard.victorMethod);
-		}
-		else if (gameBoard.victor == GameState.O){
-		    System.out.println("The Computer wins. Better Luck Next time.");
-		    System.out.println(gameBoard.victorMethod);
-		}
-		else {
-		    System.out.println("Its a draw. Better luck next time.");
-		}
-		System.out.println("Game over.");
-	}	
-
+    static int colPos;
+    static GameState gameBoard;
+    static MinMax com;
+    
+    public static void main(String[] args) {	
+        com = new MinMax(State.O);
+	gameBoard = new State();
+        Gui gui = new Gui(gameBoard);
+        while(!gameBoard.ifGameOver()) {
+            System.out.println();
+            switch (gameBoard.lastLetterUsed) {
+                case GameState.X:
+                    GamePlay comMove = com.getNextMove(gameBoard);
+                    gui.replace(comMove.col);
+                    gameBoard.makeMovement(comMove.col, GameState.O);
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (gameBoard.victor == GameState.X){
+            Wingui play = new Wingui("Player");
+        }
+        else if (gameBoard.victor == GameState.O){
+            Wingui com = new Wingui("Com");
+        }
+        else{
+            Wingui draw = new Wingui("Draw");
+        }
+    }
 }
